f58a15b47c017ca687433dcc9e2c11dca7507d0b
psxzz <m.martusevich@yandex.ru>
2021-11-10 17:01:19 +0300

    upd: sql scripts, classes

--- src/classes.py
+++ src/classes.py
@@ -1,29 +1,41 @@
 # Base class for person classes
 class Person(object):
-    def __init__(self):
-        pass
+    def __init__(self, lastname, firstname, secondname, phone, department):
+        self._last_name = lastname
+        self._first_name = firstname
+        self._second_name = secondname
+        self._phone_number = phone
+        self._department = department
 
 
 class Student(Person):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, lastname, firstname, secondname, phone, department, birthdate, gradebook):
+        super().__init__(lastname, firstname, secondname, phone, department)
+        self._birth_date = birthdate
+        self._gradebook_number = gradebook
 
 
 class Teacher(Person):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, id, lastname, firstname, secondname, phone, department):
+        super().__init__(lastname, firstname, secondname, phone, department)
+        self._id = id
 
 # Base class for structure classes 
 class Structure(object):
-    def __init__(self):
-        pass
+    def __init__(self, id, fullname, shortname):
+        self._id = id
+        self._full_name = fullname
+        self._short_name = shortname
 
 
 class Department(Structure):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, id, fullname, shortname, head):
+        super().__init__(id, fullname, shortname)
+        self._department_head = head
 
 
 class Group(Structure):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, id, fullname, shortname, students, department):
+        super().__init__(id, fullname, shortname)
+        self._students_count = students
+        self._department = department
--- src/tables.sql
+++ src/tables.sql
@@ -1,33 +1,44 @@
+CREATE DATABASE hr_university
+USE hr_university
+GO
+
 CREATE TABLE departments (
     id INT PRIMARY KEY IDENTITY,
-    full_name VARCHAR(100),
-    short_name VARCHAR(10),
-    head_id INT REFERENCES teachers(id)
+    full_name NVARCHAR(100),
+    short_name NVARCHAR(10),
+    head_id INT NOT NULL --FOREIGN KEY REFERENCES teachers(id)
 )
 GO
 
 CREATE TABLE teachers (
     id INT PRIMARY KEY IDENTITY,
-    first_name VARCHAR(15),
-    second_name VARCHAR(15),
-    last_name VARCHAR(15),
-    department_id INT REFERENCES departments(id)
+    first_name NVARCHAR(15),
+    second_name NVARCHAR(15),
+    last_name NVARCHAR(15),
+    phone_number NVARCHAR(12),
+    department_id INT FOREIGN KEY REFERENCES departments(id)
 )
+GO
+
+ALTER TABLE departments ADD FOREIGN KEY (head_id) REFERENCES teachers(id)
 GO
 
 CREATE TABLE groups (
     id INT PRIMARY KEY IDENTITY,
-    full_name VARCHAR(100),
-    short_name VARCHAR(10),
+    full_name NVARCHAR(100),
+    short_name NVARCHAR(10),
+    student_count INT DEFAULT 0,
     department_id INT REFERENCES departments(id)
 )
 GO
 
 CREATE TABLE students (
-    id INT PRIMARY KEY IDENTITY,
-    first_name VARCHAR(15),
-    second_name VARCHAR(15),
-    last_name VARCHAR(15),
+    gradebook_number INT PRIMARY KEY IDENTITY,
+    first_name NVARCHAR(15),
+    second_name NVARCHAR(15),
+    last_name NVARCHAR(15),
+    phone_number NVARCHAR(12),
+    birth_date DATE,
     group_id INT REFERENCES groups(id)
 )
 GO
